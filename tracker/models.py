bf00f497b1956e461f0c84732a27fbb39147399c
Prakhar Pratap <prakharpratap2001@gmail.com>
2024-07-02 13:36:18 +0530

    wrote tests using pytest

--- tracker/models.py
+++ tracker/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from .managers import TransactionQuerySet
 
 
 class User(AbstractUser):
@@ -26,6 +27,9 @@ class Transaction(models.Model):
     type = models.CharField(max_length=7, choices=TRANSACTION_TYPE_CHOICES)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField()
+
+    # django docs Managers
+    objects = TransactionQuerySet.as_manager()
 
     def __str__(self) -> str:
         return f"{self.type} of {self.amount} on {self.date} by {self.user}"
