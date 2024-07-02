ce7b1644d1f550b27043eaba20d1f70365408cf6
Prakhar Pratap <prakharpratap2001@gmail.com>
2024-07-02 17:46:43 +0530

    feature: transactions can be added by the user now

--- tracker/urls.py
+++ tracker/urls.py
@@ -5,4 +5,5 @@ from tracker import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("transactions/", views.transactions_list, name="transactions-list"),
+    path("transactions/create/", views.create_transaction, name="create-transaction"),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from tracker.models import Transaction
 from tracker.filters import TransactionFilter
+from tracker.forms import TransactionForm
 
 
 def index(request):
@@ -18,6 +19,7 @@ def transactions_list(request):
     )
     total_income = transaction_filter.qs.get_total_income()
     total_expenses = transaction_filter.qs.get_total_expenses()
+
     context = {
         "filter": transaction_filter,
         "total_income": total_income,
@@ -33,3 +35,19 @@ def transactions_list(request):
         )
 
     return render(request, "tracker/transactions-list.html", context)
+
+
+@login_required
+def create_transaction(request):
+    if request.method == "POST":
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            transaction = form.save(commit=False)
+            transaction.user = request.user
+            transaction.save()
+            context = {"message": "Transaction was added successfully!"}
+            return render(request, "tracker/partials/transaction-success.html", context)
+
+    context = {"form": TransactionForm()}
+
+    return render(request, "tracker/partials/create-transaction.html", context)
