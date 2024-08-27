4441fc856f0782b66875f8b42a8a50ec55d07d05
Prakhar Pratap <prakharpratap2001@gmail.com>
2024-08-27 12:22:31 +0530

    model enhancement

--- tracker/models.py
+++ tracker/models.py
@@ -8,6 +8,10 @@ class User(AbstractUser):
 
 
 class Category(models.Model):
+    """
+    Category model for storing the transaction categories with the following fields:
+    - name: CharField for storing the category name
+    """
     name = models.CharField(max_length=50, unique=True)
 
     class Meta:
@@ -18,6 +22,13 @@ class Category(models.Model):
 
 
 class Transaction(models.Model):
+    """
+    Transaction model for storing the user transactions data with the following fields:
+    - user: ForeignKey to the User model
+    - category: ForeignKey to the Category model
+    - type: CharField with choices of "income" and "expense"
+    - amount: DecimalField for storing the transaction amount
+    """
     TRANSACTION_TYPE_CHOICES = (
         ("income", "Income"),
         ("expense", "Expense"),
